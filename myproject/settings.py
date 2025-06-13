7264fc53525b7bb049e3ee26caac7f810e3743cd
B.Bharath kumar <imbharath464@gmail.com>
2025-06-13 13:52:43 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 import google.generativeai as genai
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyDI8cKeRJ0EzNbOMtJiukOgw8fx6bMXUlM")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "your_api_key")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
